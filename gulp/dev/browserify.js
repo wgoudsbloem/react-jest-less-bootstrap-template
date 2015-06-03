a4e931b4c41c72e6b7e60102635b2574cd1af4ad
= <=>
2015-06-03 14:32:07 -0400

    react(ify) is working now, this whole thing rocks!

--- gulp/dev/browserify.js
+++ gulp/dev/browserify.js
@@ -4,10 +4,22 @@ var reactify = require('reactify');
 var source = require('vinyl-source-stream');
 
 module.exports = function(){
-  b = browserify();
-  b.transform(reactify); // use the reactify transform
-  b.add('./src/jsx/index.jsx');
-  return b.bundle()
+
+  var bundler = browserify();
+
+  b = browserify(
+    {
+      entries: ['./src/jsx/Main.js'], // Only need initial file, browserify finds the deps
+      transform: [reactify],
+      global: true, // We want to convert JSX to normal javascript
+      debug: true, // Gives us sourcemapping
+      cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
+  }
+
+);
+  // b.transform(reactify); // use the reactify transform
+  // b.add('./src/jsx/index.jsx');
+   return b.bundle()
     .pipe(source('index.js'))
     .pipe(gulp.dest('./public/lib/jsx/'));
 };
